a8842c8a7277075dd7b01a999592dbd25c8fbb63
MichealRonan <132925162+MichealRonan@users.noreply.github.com>
2023-12-19 16:14:10 -0600

    Polar bear and snowman Movement

---
+++ Assets/Scripts/MeEnemies.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class MeEnemies : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Transform Ground;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    private float speed = 3f;
+    private bool isRight = true;
+    private Animator animator;
+   
+
+    private void Start() {
+        //animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+
+    void Update()
+    {
+        rb.velocity = new Vector2( horizontal * speed,0);
+        if(horizontal != 0){
+            //animator.SetBool("isWalking", true);
+        }
+        else{
+            //animator.SetBool("isWalking", false);
+        }
+         if (!isRight && horizontal > 0f){
+            flip();
+
+         }
+         else if (isRight && horizontal < 0f){
+            flip();
+         }
+
+    }
+
+    private bool isGrounded(){
+        return Physics2D.OverlapCircle(Ground.position,0.1f,groundLayer);
+    }
+
+    private void flip(){
+        isRight = !isRight;
+        Vector2 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
